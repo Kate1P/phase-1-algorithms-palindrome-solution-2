52de95109b58a024b3aa5a32458c12b6c5f5edd0
Kate Popova <popova1ea@gmail.com>
2022-12-23 20:52:13 -0800

    commit tree aa157aa915187255251d58a4dfe19c9dc7947193

--- index.js
+++ index.js
@@ -1,9 +1,25 @@
 function isPalindrome(word) {
   // Write your algorithm here
+for ( let i = 0; i <word.length/2; i++) {
+  const j = word.length - 1 - i;
+  const startChar = word[i];
+  const endChar = word[j];
+  if (startChar !== endChar) return false;
+}
+return true;
 }
 
 /* 
   Add your pseudocode here
+that means if the first letter is the same as last letter, and the second letter is the same
+as the second latter to last letter, etc. yntil we reach the middle, return true.
+
+iterate from the beginning to the middle
+check each letter to the corresponding letter from the end
+if any letters don't match, return false
+
+return true
+
 */
 
 /*
@@ -15,6 +31,10 @@ if (require.main === module) {
   // add your own custom tests in here
   console.log("Expecting: true");
   console.log("=>", isPalindrome("racecar"));
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("wow"));
+  
 
   console.log("");
 
